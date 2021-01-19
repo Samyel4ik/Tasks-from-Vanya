b756e587c0aa339bea5883573ba51df9a16bb3a7
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-01-19 19:15:16 +0300

    four

--- src/Task1/While.java
+++ src/Task1/While.java
@@ -1,32 +1,29 @@
 package Task1;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class While {
     public static void main(String[] args) {
         // Вводить последовательность чисел до тех пор, пока их сумма не достигнет М(М
         //вводится и больше 0).Ввести, какое количество чисел составили искомую сумму
         //(саму сумму тоже).
-        int m = 15;
-        int i = 0;
-        int sum = 0;
-        int z = 0; //количество числе.
 
-        while (i >= 0) {
+        Scanner num = new Scanner(System.in);
+        int number;
+        System.out.print("Введите значение: ");
+        number = num.nextInt();                 // значение которое вводит пользователь
 
+        int sum =0;
+        int  i = 0; //количество числе которое состовляло сумму
+
+        while (number>sum){
+
+            System.out.print("Введите значение: ");
+            int t = num.nextInt();
+            sum = sum+t;
             i++;
-            z++;
-            System.out.print(i + " ");
-            sum = sum + i;
-
-
-            if (sum >= m) {
-                sum = sum;
-                i = i;
-                break;
-            }
         }
-        System.out.println("Сумма чисел " + sum);
-        System.out.println("Количество числе составивших сумму" + z);
+        System.out.println("Количество чисел" + i);
     }
 }
